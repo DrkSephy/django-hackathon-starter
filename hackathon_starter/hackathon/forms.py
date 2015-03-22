4c6a5683fbe86c86a9da1b1f557d0366530ff03c
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-22 15:29:08 -0400

    Resolving #37

---
+++ hackathon_starter/hackathon/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from hackathon.models import UserProfile
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
