9da584ea9625808e8c7428e0996cb865efdd0561
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-22 15:30:29 -0400

    Resolving #38

--- hackathon_starter/hackathon/models.py
+++ hackathon_starter/hackathon/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+
+    # Override the __unicode__() method to return out something meaningful!
+    def __unicode__(self):
+        return self.user.username
