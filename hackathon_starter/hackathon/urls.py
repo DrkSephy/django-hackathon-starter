6596619998f8697bf6ef6e7ff8f4f0d8f4caa053
Eswari Pravallika Swarna <eswarna000@citymail.cuny.edu>
2015-03-30 20:14:10 +0000

    created linkedin.html

---
+++ hackathon_starter/hackathon/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, url
+
+from hackathon import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
+    url(r'^test/$', views.test, name='test'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='login'),
+    url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^api/$', views.api_examples, name='api'),
+    url(r'^steam/$', views.steam, name='steam'),
+    url(r'^github/$', views.github, name='github'),
+    url(r'^tumblr/$', views.tumblr, name='tumblr')
+)
