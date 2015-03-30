5eacb47b21418264c9bde054efb7685d5eee7096
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-30 14:17:54 -0400

    Parsing user data

--- hackathon_starter/hackathon/scripts/github.py
+++ hackathon_starter/hackathon/scripts/github.py
@@ -17,7 +17,19 @@ def getUserData():
 	req = requests.get(API_USERS_URL)
 	jsonList = []
 	jsonList.append(json.loads(req.content))
-	print jsonList
+	parsedData = []
+	userData = {}
+	for data in jsonList: 
+		userData['name'] = data['name']
+		userData['blog'] = data['blog']
+		userData['email'] = data['email']
+		userData['public_gists'] = data['public_gists']
+		userData['public_repos'] = data['public_repos']
+		userData['avatar_url'] = data['avatar_url']
+		userData['followers'] = data['followers']
+		userData['following'] = data['following']
+	parsedData.append(userData)
+	return parsedData
 	
 	
 	
