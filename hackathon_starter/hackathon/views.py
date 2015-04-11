28729d24d62cf5e5ca643d3231cbe558ea2ac11b
David Leonard <sephirothcloud1025@yahoo.com>
2015-04-11 19:44:02 -0400

    Removing images from api_examples template, adding JSONResponse comment for Github view

--- hackathon_starter/hackathon/views.py
+++ hackathon_starter/hackathon/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 # Scripts
 from scripts.steam import gamesPulling, steamIDPulling 
@@ -108,7 +109,7 @@ def github(request):
     allData['filteredData'] = filtered
     allData['filteredStargazers'] = filteredStargazers
     allData['forkedRepos'] = forkedRepos
-
+    # return JsonResponse({'data': allData});
     return render(request, 'hackathon/github.html', { 'data': allData })
 
 def tumblr(request):
