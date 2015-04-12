ed3c027403e7c371486f0b9d6ec095aa6204f883
David Leonard <sephirothcloud1025@yahoo.com>
2015-04-12 17:43:03 -0400

    Re-adding Ionic project, fixing project name and attributes of project directory

---
+++ ionic/www/js/app.js
@@ -0,0 +1,72 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "templates/menu.html",
+    controller: 'AppCtrl'
+  })
+
+  .state('app.search', {
+    url: "/search",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/search.html"
+      }
+    }
+  })
+
+  .state('app.browse', {
+    url: "/browse",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/browse.html"
+      }
+    }
+  })
+    .state('app.sessions', {
+    url: "/sessions",
+    views: {
+        'menuContent': {
+            templateUrl: "templates/sessions.html",
+            controller: 'SessionsCtrl'
+        }
+    }
+  })
+
+  .state('app.session', {
+    url: "/sessions/:sessionId",
+    views: {
+        'menuContent': {
+          templateUrl: "templates/session.html",
+          controller: 'SessionCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/sessions');
+});
