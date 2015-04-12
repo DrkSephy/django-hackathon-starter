d2b6d4b39fba7bdc5c1aa4d53e1877adb5c695ce
David Leonard <sephirothcloud1025@yahoo.com>
2015-04-11 22:59:55 -0400

    Adding main controller for Ionic routes

---
+++ ionic/www/js/app.js
@@ -0,0 +1,39 @@
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "templates/menu.html",
+    controller: 'AppCtrl'
+  })
+
+  .state('app.sessions', {
+    url: "/sessions",
+    views: {
+        'menuContent': {
+            templateUrl: "templates/sessions.html",
+            controller: 'SessionsCtrl'
+        }
+    }
+  })
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/sessions');
+});
