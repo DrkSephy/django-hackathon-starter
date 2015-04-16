7a9324998b25333082d676fe28662c869704e5b3
David Leonard <sephirothcloud1025@yahoo.com>
2015-04-16 14:59:42 -0400

    Fixing paths in AngularJS description for readme, working on new census data

--- hackathon_starter/hackathon/scripts/census.py
+++ hackathon_starter/hackathon/scripts/census.py
@@ -12,9 +12,9 @@ import simplejson as json
 
 API_BASE_URL = 'http://api.census.gov/data/2013/pep/natstprc'
 
-def getPopulationEstimate():
-	url = 'http://api.census.gov/data/2013/pep/natstprc18?get=POPEST18PLUS2013&for=us&key=1286203d2772de1f09f13392cc42a0197b2cd414'
+def getPopulationEstimate(url):
 	req = requests.get(url)
-	print req.text
+	print len(req.text)
+	#print req.text
 
-getPopulationEstimate()
+getPopulationEstimate('http://api.census.gov/data/2013/pep/monthlynatchar5?get=AGE,SEX,DATE,RACE5,HISP,POP,UNIVERSE&key=1286203d2772de1f09f13392cc42a0197b2cd414')
