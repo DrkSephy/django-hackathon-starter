504ef22502933c79778ccd7fb7ba206fa0948e4a
David Leonard <sephirothcloud1025@yahoo.com>
2015-04-19 17:55:51 -0400

    Adding integration test for API Examples view

--- hackathon_starter/hackathon/tests.py
+++ hackathon_starter/hackathon/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class HackathonViewsTestCase(TestCase):
+	def testIndex(self):
+		resp = self.client.get('/hackathon/api/')
+		self.assertEqual(resp.status_code, 200)
