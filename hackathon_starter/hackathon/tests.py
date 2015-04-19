96fbbc7e117953ba1a716518a0b5645467ca6775
David Leonard <sephirothcloud1025@yahoo.com>
2015-04-19 17:57:40 -0400

    Adding integration test for steam endpoint

--- hackathon_starter/hackathon/tests.py
+++ hackathon_starter/hackathon/tests.py
@@ -6,3 +6,7 @@ class HackathonViewsTestCase(TestCase):
 	def testIndex(self):
 		resp = self.client.get('/hackathon/api/')
 		self.assertEqual(resp.status_code, 200)
+
+	def testSteam(self):
+		resp = self.client.get('/hackathon/steam')
+		self.assertEqual(resp.status_code, 301)
