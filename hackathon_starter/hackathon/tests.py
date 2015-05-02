047ef8f7efc9a89f61374e9acc6b2a3f25817e33
David Leonard <sephirothcloud1025@yahoo.com>
2015-05-02 16:49:23 -0400

    Adding tests for New York Times endpoints

--- hackathon_starter/hackathon/tests.py
+++ hackathon_starter/hackathon/tests.py
@@ -107,5 +107,21 @@ class TwilioTestCase(TestCase):
 	#	resp = self.client.post('/hackathon/twilio/', {'number': '+13473282978', 'message': 'hello world'})
 	#	self.assertEqual(resp.status_code, 302)
 
+class NewYorkTimesTestCase(TestCase):
+	def testPopularArticles(self):
+		resp = self.client.get('/hackathon/nytimespop/')
+		self.assertEqual(resp.status_code, 200)
+
+	def testPopularArticlesContent(self):
+		resp = self.client.get('/hackathon/nytimespop/')
+		self.assertNotEqual(resp.content, '')
+
+	def testTopArticles(self):
+		resp = self.client.get('/hackathon/nytimestop/')
+		self.assertEqual(resp.status_code, 200)
+
+	def testTopArticlesContent(self):
+		resp = self.client.get('/hackathon/nytimestop/')
+		self.assertNotEqual(resp.content, '')
 
 
