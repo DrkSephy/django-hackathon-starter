ee2e6516f666b65158abafe119617885614e116c
Marco Quezada <Qmarco88@gmail.com>
2015-05-05 18:13:33 -0400

    Added stockdatapull method and rdiff data method to Quandl.py

--- hackathon_starter/hackathon/scripts/steam.py
+++ hackathon_starter/hackathon/scripts/steam.py
@@ -26,30 +26,3 @@ def steamidpulling(steamun, apikey):
     data = json.loads(req.content)
     steamid = data['response']['steamid']
     return steamid
-
-def steamlibrarypull(steamid, apikey):
-    '''Pulls out a CSV of Steam appids.'''
-    steaminfo = {
-        'key': apikey,
-        'steamid': steamid,
-        'format':'JSON',
-        'include_appinfo':'1'
-    }
-    url = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/'
-    req = requests.get(url, params=steaminfo)
-    data = json.loads(req.content)
-    response = data['response']['games']
-    games = {}
-    for game in response:
-        url = 'http://store.steampowered.com/api/appdetails/?appids=%d&filters=price_overview&cc=us'
-        getprice = requests.get(url % game['appid'])
-        if getprice.status_code == 200:
-            rjson = json.loads(getprice.text)
-            # use the appid to fetch the value and convert to decimal
-            # appid is numeric, cast to string to lookup the price
-            try:
-                price = rjson[str(game['appid'])]['data']['price_overview']['initial'] * .01
-            except KeyError:
-                pass
-            games[game['name']] = {'price': price, 'appid': game['appid']}
-    return games
